58f007589402c209061e9faf784ded07fc30e6be
Abderrahmen <abder78@gmail.com>
2021-06-06 00:38:10 +0200

    Updated board e2e test

--- app/tests/e2e/specs/board.js
+++ app/tests/e2e/specs/board.js
@@ -4,7 +4,12 @@ describe("Test boards", () => {
   it("Create a board", () => {
     const name = "Board name here - " + Date.now();
     cy.visit("/");
-    cy.get("#login-username").type("test");
+    const username = "test-" + Date.now();
+    cy.get("#register-username").type(username);
+    cy.get("#register-password").type("test");
+    cy.get("#register-password2").type("test");
+    cy.get("#create-account").click();
+    cy.get("#login-username").type(username);
     cy.get("#login-password").type("test");
     cy.get("#login").click();
     cy.get("[data-nav='nav-boards']").click();
